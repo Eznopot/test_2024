acbcf85cdde5e4e9518b01cdf9844c658fdbd98b
remy <remysalem@hotmail.fr>
2024-05-31 10:10:45 +0200

    [+] ehehe

--- test1/main.go
+++ test1/main.go
@@ -9,15 +9,6 @@ import (
 // fonction a implémenter
 func FolderFilesFinder(folderpath string) ([]utils.BoxFile, error) {
 	var res []utils.BoxFile
-	filesFolders, _ := utils.GetFilesinFolder(folderpath)
-	for _, tmp := range filesFolders {
-		if utils.IsFileADir(tmp) {
-			f, _ := FolderFilesFinder(folderpath + tmp.Name())
-			res = append(res, f...)
-		}
-		checksum, _ := utils.GetChecksum(folderpath + tmp.Name())
-		res = append(res, utils.BoxFile{PathBox: folderpath + tmp.Name(), Checksum: checksum, UpdatedAt: tmp.ModTime()})
-	}
 	return res, nil
 }
 
